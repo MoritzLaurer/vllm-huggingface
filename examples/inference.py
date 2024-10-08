8b5cc0ba0742998754643a740fd3235541ac105e
moritzlaurer <moritz.laurer@posteo.de>
2024-10-08 15:21:58 +0200

    simplify inference example

--- examples/inference.py
+++ examples/inference.py
@@ -2,33 +2,35 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 from time import time
-from multiprocessing.pool import ThreadPool
-
 
 load_dotenv() 
 ENDPOINT_URL = os.getenv("HF_ENDPOINT_URL") + "/v1/" # if endpoint object is not available check the UI 
 API_KEY = os.getenv("HF_TOKEN")
-STREAM = False
 
 # initialize the client but point it to TGI
 client = OpenAI(base_url=ENDPOINT_URL, api_key=API_KEY)
 
+generation_parameters = {
+    "temperature": 0.2,
+    "max_tokens": 128,
+    "top_p": 0.7,
+    "stream": False,
+}
 
 def predict(messages):
     return client.chat.completions.create(
         model="/repository", # needs to be /repository since there are the model artifacts stored
         messages=messages,
-        max_tokens=30,
-        temperature=0.0,
-        stream=False,
+        **generation_parameters
     ).choices[0].message.content
 
-if __name__ == "__main__":
-    
-    batch_size = 64
-    pool = ThreadPool(batch_size)
+
+if __name__ == "__main__":    
 
     messages = [
+        {"role": "system", 
+         "content": "You are a helpful assistant",
+        },
         {"role": "user", "content": [
             {
                 "type": "image_url",
@@ -38,16 +40,14 @@ if __name__ == "__main__":
             },
             {
                 "type": "text",
-                "text": "What is in the above image? Explain in detail."
+                "text": "What type of animal is in the image above? What color does it have?"
+                #"text": "\nPlease provide the bounding box coordinate of the region this sentence describes: <ref>the bird</ref>"  #"What is in the above image? Explain in detail."
             }
         ]},
     ]
 
     start = time()
-    responses = pool.map(predict, [messages] * batch_size)
-    print(responses[0])
+    response = predict(messages)
+    print(f"LLM output: {response}")
     print(f"Time taken: {time() - start:.2f}s")
 
-    start = time()
-    responses = list(map(predict, [messages] * batch_size))
-    print(f"Time taken: {time() - start:.2f}s")
